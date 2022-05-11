02107a189144e660ff692214d95118ee162b932f
farley0909 <80477605+farley0909@users.noreply.github.com>
2022-05-11 13:37:28 -0300

    Create Quest4BM.java

---
+++ Quest4BM.java
@@ -0,0 +1,8 @@
+public class Quest4BM{
+public static void main(String[] args){
+   long long1 = 100;
+   int int1 =200;
+   int int2 = log1+int1;
+  }
+}
+// Ocorre um erro devido os tipos diferirem de tamanho em bytes
