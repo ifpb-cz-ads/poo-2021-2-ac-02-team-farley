d1a6b574548464f2f040e2c1e18c5231ee1f2e30
farley0909 <80477605+farley0909@users.noreply.github.com>
2022-05-11 11:06:24 -0300

    Create Quest2CBB.java

---
+++ Quest2CBB.java
@@ -0,0 +1,10 @@
+public class Quest2CBB{
+  public static void main(String[] args){
+   int n =2;
+   int n2 = 3;
+   int n3 = 1;
+   System.out.printf("A soma eh: %d", (n+n2+n3));
+   System.out.printf("A media eh: %d", (n+n2+n3)/3);
+  }
+
+}
